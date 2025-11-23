29b553bb136d96816ed3eddbaf412d107ec56ddb
NVB20 <nivb49@gmail.com>
2025-11-23 20:17:40 +0200

    mongo connection

--- src/etl/etl.py
+++ src/etl/etl.py
@@ -1,11 +1,13 @@
 from src.etl.extract import open_whatsapp
 from src.etl.transform import process_messages
-
+from src.etl.db.mongodb.mongo_connect import get_mongo_connection
 
 def run_etl():
     messages = open_whatsapp()
-    if messages != 0:
+    if messages == 0:
+        print("failed to read messages")
+    else:
         messages = process_messages(messages)
         print(messages)
-    else:
-        print("failed to read messages")
+        mongo = get_mongo_connection()
+        db = mongo.get_database()    
