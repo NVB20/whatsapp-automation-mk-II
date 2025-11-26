b8b772e3dd1aec81a245a5659d5a8853c6b9ec70
NVB20 <nivb49@gmail.com>
2025-11-27 00:41:51 +0200

    updated def

--- src/etl/db/mongodb/message_saver.py
+++ src/etl/db/mongodb/message_saver.py
@@ -10,7 +10,7 @@ class MessageSaver:
     def __init__(self):
         """Initialize connection and setup unique index"""
         self.mongo = get_mongo_connection()
-        self.collection = self.mongo.get_messages_collection()
+        self.collection = self.mongo.get_students_messages_collection()
         self._setup_unique_index()
     
     def _setup_unique_index(self):
